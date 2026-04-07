a799e04b347c5d005e91a44275f15413fb4eaa1e
Lunar0769 <pkavya085@gmail.com>
2026-04-07 11:54:08 +0530

    Add Vercel Analytics to app layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import DottedSurface from '../components/DottedSurface'
+import { Analytics } from '@vercel/analytics/next'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <DottedSurface />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
